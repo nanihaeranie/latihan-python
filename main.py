ba3aa457cb8aec50f3167f86fc5326d2c4c9d9a5
nanihaeranie <nanihaerani255@gmail.com>
2024-10-05 15:11:32 +0700

    add repository

---
+++ main.py
@@ -0,0 +1,22 @@
+nama_lengkap = "Nama Nani haaerani"
+nim = 123
+prodi = "Teknik Informatika Sistem Informasi"
+nilai = 8.5
+lulus_ujian =True
+
+print("Nama:", nama_lengkap)
+print("NIM:", nim)
+print("Prodi:", prodi)
+print("Nilai:", nilai)
+print("status ujian:", lulus_ujian)
+
+print 
+nama_lengkap = "Nama Nani Haaerani"
+nim = 123
+prodi = "Teknik Informatika Sistem Informasi"
+nilai = 8.5
+lulus_ujian = True
+
+
+
+output = "nama: " +  nama_lengkap +"/nNIM: " + Str(nim) + "/nprodi: + prodi + "/nNilai: " + str(nilai) + /nStatus Ujian: + str()
---
+++ main3.py
@@ -0,0 +1,38 @@
+# mendefinisikan verbal
+angka1 = 10
+angka2 = 5 
+angka3 = 2
+
+
+# # operasi penjumlahan 
+# hasil_penjumlahan = angka1 + angka2 + angka3
+# print ("hasil penjumlahan:", hasil_penjumlahan)
+
+# # operasi pengurangan 
+# hasil_pengurangan = angka1 - angka2 - angka3
+# print("hasil pengurangan:", hasil_pengurangan)
+
+# # operasi perkalian 
+# hasil_perkalian = angka1 * angka2 * angka3
+# print("hasil perkalian", hasil_perkalian)
+
+# # operasi pembagian 
+# hasil_pembagian = angka1 / angka2 / angka3
+# print("hasil pembagian:", hasil_pembagian)
+
+# # operasi pemanagkatan
+# hasil_pemangkatan = angka1 ** angka2 ** angka3
+# print("hasil pemangkatan:", hasil_pemangkatan
+# print(angka1 == angka2)
+# print(angka1 != angka2)
+# print(angka1 > angka2)
+# print(angka1 < angka2)
+# print(angka1 >= angka2)
+# print(angka1 <= angka2)
+
+nilai1 = 15
+nilai2 = 10
+nilai3 = 20
+print(nilai2 == nilai3)
+print(nilai1 >= nilai3)
+print(nilai1 < nilai3)
